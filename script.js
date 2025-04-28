c68114c113609459368663ed8f882aeae845854d
listn <186027681+Klez-src@users.noreply.github.com>
2025-04-28 21:01:46 +0100

    :p

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// Optional: Add any JavaScript functionality here
 document.addEventListener("DOMContentLoaded", function() {
     const animatedTexts = document.querySelectorAll('.animated-text');
     animatedTexts.forEach((text, index) => {
-        text.style.animationDelay = `${index * 0.2}s`;
-        text.style.opacity = 1; // Make text visible after animation
-    });
-});
